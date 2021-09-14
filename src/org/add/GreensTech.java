ba39e372785d719922093726d572d24c21ba4c25
RajiDinesh <d.rajeswari.work@gmail.com>
2021-09-14 19:04:52 +0530

    Done

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,19 @@
+package org.add;
+
+public class GreensTech {
+	public String greensOmr()
+	{
+		return "omr";
+	}
+	public String Adyar()
+	{
+		return "Adyar";
+	}
+
+public static void main(String[]args) {
+	GreensTech a=new GreensTech();
+	System.out.println(a.Adyar());
+	
+}
+	
+}
