536620ca3c438128e8365961e8edf87583f7b13a
RajiDinesh <d.rajeswari.work@gmail.com>
2021-09-17 22:16:33 +0530

    class and new methods added

---
+++ src/org/add/Class2.java
@@ -0,0 +1,13 @@
+package org.add;
+
+public class Class2 {
+	public void method1() {
+		System.out.println("method1");
+	}
+	public void method2() {
+		System.out.println("method2");
+	}
+	public void method3() {
+		System.out.println("method3");
+	}
+}
